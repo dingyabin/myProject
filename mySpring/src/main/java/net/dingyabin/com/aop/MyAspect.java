3f23118844625866ed3500537a7e26552a586af0
dingyabin <1262027459@qq.com>
2017-02-19 20:25:08 +0800

    aop

---
+++ mySpring/src/main/java/net/dingyabin/com/aop/MyAspect.java
@@ -0,0 +1,38 @@
+package net.dingyabin.com.aop;
+
+import net.dingyabin.com.bean.Student;
+import org.aspectj.lang.JoinPoint;
+
+
+/**
+ * Created by MrDing
+ * Date: 2017/2/19.
+ * Time:12:41
+ */
+
+public class MyAspect {
+
+
+    public void before(JoinPoint point,Student student){
+        String className = point.getTarget().getClass().getName();
+        String methodName = point.getSignature().getName();
+        Object[] args = point.getArgs();
+        System.out.println("aop的befroe方法执行......");
+        System.out.println(String.format("className=%s, methodName=%s ,参数为:%s",className,methodName,args[0]));
+        System.out.println("参数值值："+student);
+    }
+
+
+
+    public void afterReturn(JoinPoint point,Student  student){
+        String className = point.getTarget().getClass().getName();
+        String methodName = point.getSignature().getName();
+        Object[] args = point.getArgs();
+        System.out.println("aop的afterReturn方法执行......");
+        System.out.println(String.format("className=%s, methodName=%s ,参数为:%s",className,methodName,args[0]));
+        System.out.println("返回值："+student);
+
+    }
+
+
+}
--- mySpring/src/main/java/net/dingyabin/com/controller/MyController.java
+++ mySpring/src/main/java/net/dingyabin/com/controller/MyController.java
@@ -2,6 +2,8 @@ package net.dingyabin.com.controller;
 
 import net.dingyabin.com.bean.Student;
 import net.dingyabin.com.utils.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +17,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MyController {
 
+    Logger logger=LoggerFactory.getLogger(MyController.class);
 
     @RequestMapping(value="/test",produces = "application/json;charset=utf-8")
     @ResponseBody
-    public Student convertion(@RequestBody  Student stu){
+    public Student convertion(@RequestBody  Student student){
         System.out.println(Config.getAsString("test"));
         System.out.println(Config.getAsInteger("number"));
-        return stu;
+        return student;
     }
+
+
+    @RequestMapping(value="/test2",produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public Student convertion2(Student student){
+        student.setName("Jim");
+        System.out.println(Config.getAsString("test"));
+        System.out.println(Config.getAsInteger("number"));
+        return student;
+    }
+
+
+
+
 }
