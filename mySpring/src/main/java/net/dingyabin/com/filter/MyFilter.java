4f37765074c1a01277ad0e35e80b1382aec7504e
dingyabin <1262027459@qq.com>
2017-03-16 23:10:17 +0800

    oncePerRequestFilter

--- mySpring/src/main/java/net/dingyabin/com/filter/MyFilter.java
+++ mySpring/src/main/java/net/dingyabin/com/filter/MyFilter.java
@@ -1,7 +1,7 @@
 package net.dingyabin.com.filter;
 
 import org.slf4j.MDC;
-import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -15,7 +15,7 @@ import java.util.UUID;
  * Date: 2017/3/13.
  * Time:22:05
  */
-public class MyFilter extends OncePerRequestFilter {
+public class MyFilter extends CharacterEncodingFilter {
 
 
 
@@ -29,10 +29,28 @@ public class MyFilter extends OncePerRequestFilter {
      */
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        doEncoding(httpServletRequest,httpServletResponse,filterChain);
         MDC.clear();
         MDC.put("traceId", httpServletRequest.getHeader("traceId") == null ? UUID.randomUUID().toString() : httpServletRequest.getHeader("traceId"));
         httpServletResponse.addHeader("traceId",MDC.get("traceId"));
         filterChain.doFilter(httpServletRequest,httpServletResponse);
+    }
+
+
+
+
+    /**
+     * 编码过滤
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param filterChain
+     */
+    private void doEncoding(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain){
+        try {
+            super.doFilterInternal(httpServletRequest,httpServletResponse,filterChain);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
