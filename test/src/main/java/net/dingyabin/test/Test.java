dee24a6b4779112fa186fe437cb7755474e4e32c
dingyabin <1262027459@qq.com>
2018-07-26 23:18:31 +0800

    数据结构

--- test/src/main/java/net/dingyabin/test/Test.java
+++ test/src/main/java/net/dingyabin/test/Test.java
@@ -1,5 +1,6 @@
 package net.dingyabin.test;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
@@ -7,11 +8,15 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by MrDing
@@ -20,36 +25,30 @@ import java.util.concurrent.*;
  */
 public class Test {
 
+    private static final LinkedBlockingQueue<Torrent> QUEUE = new LinkedBlockingQueue<>();
+    private static final String BATHPATH = "C:\\Users\\MrDing\\Desktop\\1111\\";
+
     public static void main(String[] args) throws InterruptedException {
-        //File file = new File("C:\\Users\\MrDing\\Desktop\\新建文本文档.txt");
-        // FileUtils.write(file, text + "(" + href + ")\n", "gbk", true);
-        LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<>();
         ExecutorService executorService = Executors.newFixedThreadPool(10);
         for (int i = 1; i <= 10; i++) {
-            executorService.submit(new parseHome(i, "http://68.168.16.149/forum/forum-25-" + i + ".html", queue));
+            executorService.submit(new parseHome(i, "http://68.168.16.149/forum/forum-25-" + i + ".html"));
+            executorService.submit(new DownTorrent());
         }
         executorService.shutdown();
-        executorService.awaitTermination(10, TimeUnit.MINUTES);
+        executorService.awaitTermination(10, TimeUnit.HOURS);
     }
 
 
 
-    private static String getHtml(String url) throws IOException {
-        HttpURLConnection e = (HttpURLConnection) (new URL(url)).openConnection();
-        InputStream inputStream = e.getInputStream();
-        return IOUtils.toString(inputStream, "gbk");
-    }
 
 
     private static class parseHome implements Runnable {
         private int page;
         private String url;
-        private BlockingQueue<String> queue;
 
-        public parseHome(int page, String url, BlockingQueue<String> queue) {
+        public parseHome(int page, String url) {
             this.page = page;
             this.url = url;
-            this.queue = queue;
         }
 
         @Override
@@ -68,7 +67,7 @@ public class Test {
                             .getElementsByTag("a").get(0);
                     String text = a.text();
                     String href = "http://68.168.16.149/forum/" + a.attr("href");
-                    queue.offer(href);
+                    QUEUE.offer(new Torrent(text, href));
                     size++;
                     System.out.println(text + "(" + href + ")");
                 }
@@ -79,5 +78,82 @@ public class Test {
         }
     }
 
+
+    private static class DownTorrent implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while (!QUEUE.isEmpty()){
+                    Torrent torrent = QUEUE.poll();
+                    if (torrent == null) {
+                        continue;
+                    }
+                    Document doc = Jsoup.parse(getHtml(torrent.getUrl()));
+                    Element tAttachlist = doc.getElementsByClass("t_attachlist").get(0);
+                    Element a = tAttachlist.getElementsByTag("a").get(1);
+                    String href = "http://68.168.16.149/forum/" + a.attr("href");
+                    byte[] bytes = getFile(href);
+                    FileUtils.writeByteArrayToFile(createFile(BATHPATH + torrent.getName() + ".torrent"), bytes, false);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    private static String getHtml(String url) throws IOException {
+        HttpURLConnection e = (HttpURLConnection) (new URL(url)).openConnection();
+        InputStream inputStream = e.getInputStream();
+        String gbk = IOUtils.toString(inputStream, "gbk");
+        IOUtils.closeQuietly(inputStream);
+        return gbk;
+    }
+
+    private static byte[] getFile(String url) throws IOException {
+        HttpURLConnection e = (HttpURLConnection) (new URL(url)).openConnection();
+        InputStream inputStream = e.getInputStream();
+        byte[] bytes = IOUtils.toByteArray(inputStream);
+        IOUtils.closeQuietly(inputStream);
+        return bytes;
+    }
+
+
+    private static File createFile(String path) throws IOException {
+        File file = new File(path);
+        if (!file.exists() && !file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();
+        }
+        file.createNewFile();
+        return file;
+    }
+
+
+    private static class Torrent {
+        private String name;
+        private String url;
+
+        public Torrent(String name, String url) {
+            this.name = name;
+            this.url = url;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+    }
 
 }
